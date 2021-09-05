adc4398dfd26963277cbc7cb02fa9c2ec986f55d
Rockharrd <33941531+Rockharrd@users.noreply.github.com>
2021-09-05 10:54:32 -0700

    Added Variables

--- assignment2.php
+++ assignment2.php
@@ -1,1 +1,23 @@
-The Googleplex is the corporate headquarters complex of Google and its parent company Alphabet Inc. It is located at 1600 Amphitheatre Parkway in Mountain View, California, United States.
+<!DOCTYPE html> //Ryan King
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>The $business is the corporate headquarters complex of Google and its parent company Alphabet Inc. It is located at $street in $city, $state, $country.</p>
+</body>
+</html>
+
+
+
+
+
+
+
+$business = "Googleplex";
+$street = "1600 Amphitheatre Parkway";
+$city = "Mountain View";
+$state = "California";
+$country = "United States";
